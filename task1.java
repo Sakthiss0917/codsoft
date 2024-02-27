959a8897a21552be21bf7ebeccc77194a671b535
Sakthiss0917 <126088949+Sakthiss0917@users.noreply.github.com>
2024-02-27 11:15:40 +0530

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,49 +1,53 @@
 import java.util.Random;
 import java.util.Scanner;
-
-public class task1 {
-
+public class task1
+{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        
-        int score = 0;
+        Scanner userip = new Scanner(System.in);
+        Random generatedvalue = new Random();
+
+        int points = 0;
         int playCount = 0;
+        int attempts = 0;
+        int maxAttempts = 10;
 
         while (true) {
-            int secretNumber = random.nextInt(100) + 1;
-            int maxAttempts = 5;
-            int attempts = 0;
+            int secretNumber = generatedvalue.nextInt(100) + 1;
 
-            System.out.println("Round " + (playCount + 1));
+            System.out.println("Round " + (attempts + 1));
 
             while (true) {
                 System.out.print("Guess the number (between 1 and 100): ");
-                int guess = scanner.nextInt();
+                int guess = userip.nextInt();
 
                 if (guess == secretNumber) {
                     System.out.println("Congratulations! You guessed the correct number " + secretNumber + "!");
                     break;
+                } else if (guess > 100) {
+                    System.out.println("Enter value within the range 1 to 100");
                 } else if (guess < secretNumber) {
                     System.out.println("Your guess is too low. Try again.");
                 } else if (guess > secretNumber) {
                     System.out.println("Your guess is too high. Try again.");
+                } else if (guess > 100) {
+                    System.out.println("Enter value within the range 1 to 100");
                 }
 
                 attempts++;
 
                 if (attempts >= maxAttempts) {
-                    System.out.println("Sorry, you've run out of attempts. The correct number was " + secretNumber + ".");
+                    System.out
+                            .println("Sorry, you've run out of attempts. The correct number was " + secretNumber + ".");
                     break;
                 }
             }
 
             if (attempts < maxAttempts) {
-                score++;
+                points++;
             }
 
             System.out.print("Do you want to play again? (yes/no): ");
-            String playAgain = scanner.next();
+            String playAgain = userip.next();
 
             if (!playAgain.equalsIgnoreCase("yes")) {
                 break;
@@ -52,7 +56,7 @@ public class task1 {
             playCount++;
         }
 
-        System.out.println("Your final score is " + score + " out of " + (playCount + 1) + " rounds.");
-        scanner.close();
+        System.out.println("Your final score is " + points + " out of " + (attempts + 1) + " attempts.");
+        userip.close();
     }
 }
